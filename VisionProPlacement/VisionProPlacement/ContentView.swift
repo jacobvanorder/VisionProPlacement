5c04b60000870196ce16d4f51af93cf8b8888f69
Jacob Van Order <jacob@sushigrass.com>
2024-03-09 15:45:22 -0600

    Got rid of default reality kit content.

--- VisionProPlacement/VisionProPlacement/ContentView.swift
+++ VisionProPlacement/VisionProPlacement/ContentView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
 
@@ -19,9 +18,6 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
             Text("Hello, world!")
 
             Toggle("Show ImmersiveSpace", isOn: $showImmersiveSpace)
